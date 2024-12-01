6c657c33398d1c7d61585904efd6e14bf27e7eae
tanahiro2010 <herentongkegu087@gmail.com>
2024-12-01 14:41:47 +0900

    Update api/register

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -2,7 +2,7 @@ import fs, { readFileSync } from "fs";
 import { readFile } from "fs/promises";
 
 const DB_PATH = "./src/app/db/database.json";
-const allow_content_type = "x-www-form-urlencoded";
+const allow_content_type = "application/x-www-form-urlencoded";
 const need_keys = [
     "user_id",
     "user_name",
@@ -16,10 +16,16 @@ export async function POST(req) {
 
     if (content_type == allow_content_type) { // APIのContent-Typeを制限
         const body = await req.text();
-        let params;
+        console.log(body);
+        let params = {};
         try {
-            params = new URLSearchParams(body); // ちゃんとデータが読み取れる方式か
+            const temp = new URLSearchParams(body); // ちゃんとデータが読み取れる方式か
+            params = Object.fromEntries(temp.entries());
         } catch (e) {
+            console.log({
+                "sccess": false,
+                "message": "Parameter needs user_id and user_name and user_pass and user_mail."
+            });
             return new Response(
                 JSON.stringify({
                     "sccess": false,
@@ -28,8 +34,12 @@ export async function POST(req) {
             );
         }
 
+        console.log(params);
+
         const keys = Object.keys(params);
-        const need_include = false;
+        let need_include = false;
+
+        console.log(JSON.stringify(keys));
 
         for (let need_key of need_keys) {
             if (!keys.includes(need_key)) {
@@ -38,6 +48,10 @@ export async function POST(req) {
         }
 
         if (need_include) { // 必要なパラメータがどれか足りなかったら
+            console.log({
+                "sccess": false,
+                "message": "Parameter needs user_id and user_name and user_pass and user_mail."
+            });
             return new Response(
                 JSON.stringify({
                     "sccess": false,
@@ -60,6 +74,10 @@ export async function POST(req) {
         const user_ids = Object.keys(users_data);
 
         if (user_ids.includes(user_id)) {
+            console.log({
+                "sccess": false,
+                "message": "The user id used."
+            });
             return new Response(JSON.parse({
                 "sccess": false,
                 "message": "The user id used."
@@ -76,7 +94,8 @@ export async function POST(req) {
             "user_id":   user_id,
             "user_name": user_name,
             "user_mail": user_mail,
-            "user_pass": user_pass
+            "user_pass": user_pass,
+            "user_photos": []
         };
 
         database['user'][user_id] = userObj;
